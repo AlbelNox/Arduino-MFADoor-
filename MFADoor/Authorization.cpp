20b257c3b33c9e160b1512a9d2d50ebc0a3a0ab3
mthiele <mthiele@hellmann.com>
2025-06-03 09:16:42 +0200

    modified class,   adding pointer for display und keypadmanager to support methods

--- MFADoor/Authorization.cpp
+++ MFADoor/Authorization.cpp
@@ -1,6 +1,11 @@
 #include "Authorization.h"
-Authorization::Authorization(const char* jsonData) {
+#include "Display.h"
+#include "KeypadManager.h"
+
+Authorization::Authorization(const char* jsonData, Display* display, KeypadManager* keypadManager) {
   _json = jsonData;
+  _display = display;
+  _keypadManager = keypadManager;
 }
 
 bool Authorization::isUIDAuthorized(const String& uid) {
@@ -37,4 +42,26 @@ String Authorization::getNameFromUID(const String& uid) {
     }
   }
   return "";
+}
+
+void Authorization::checkRfid(const String& uidStr){
+  if (isUIDAuthorized(uidStr)) {
+    String vorname = getNameFromUID(uidStr);
+    _display->greeting(vorname);
+    String pin = _keypadManager->keypadInput(vorname);
+    if(pin.length() == 4){
+    checkPin(uidStr, pin, vorname);
+    } else {
+      _display->pinIncorrect();
+    }
+  }
+}
+
+void Authorization::checkPin(const String& uidStr, const String& pin, const String& vorname){
+  bool pinIsCorrect = isPinCorrect(uidStr, pin);
+    if(pinIsCorrect){
+      _display->pinCorrect(vorname);
+    } else{
+      _display->pinIncorrect();
+    }
 }
--- MFADoor/Authorization.h
+++ MFADoor/Authorization.h
@@ -1,18 +1,19 @@
-#ifndef AUTHORIZATION_H
-#define AUTHORIZATION_H
-
 #include <ArduinoJson.h>
+#include "Display.h"
+#include "KeypadManager.h"
 
 class Authorization {
   public:
-    Authorization(const char* jsonData);
+    Authorization(const char* jsonData, Display* display, KeypadManager* keypadManager);
 
     bool isUIDAuthorized(const String& uid);
     bool isPinCorrect(const String& uid, const String& pin);
     String getNameFromUID(const String& uid);
-  
+    void checkRfid(const String& uidStr);
+    void checkPin(const String& uidStr, const String& pin, const String& vorname);
+
   private:
     const char* _json;
+    Display* _display;  // Zeiger auf Display-Instanz
+    KeypadManager* _keypadManager;
 };
-
-#endif
